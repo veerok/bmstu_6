ecc52262fe5f13153fc94c570463b1809a7a9f27
veerok <vselivanova4@gmail.com>
2016-05-29 21:39:10 +0300

    4b

---
+++ 04/b.pas
@@ -0,0 +1,62 @@
+program main;
+
+{$mode objfpc}{$H+}
+
+uses math;
+
+
+function getFac(iterations: integer): double;
+var
+  i: integer;
+  sum: double;
+begin
+  sum := 0;
+
+  for i:=1 to iterations+1 do
+  begin
+    sum := sum + 1 / (i*(i+1)*(i+2)*(i+3));
+  end;
+  result := sum;
+end;
+
+
+var
+  result, sample: double;
+  counter: integer;
+begin
+
+  // 9,073415289388
+  sample := 0.05;
+  counter := 1;
+
+  repeat
+    result := getFac(counter);
+    writeLn('Iteration ', counter, ' result ', result);
+    inc(counter);
+  until result > sample;
+
+end.
+
+
+
+
+
+// var result = 0;
+// var sample = 0.055555555;
+// 
+// function fack(iterationAmount) {
+// 	var sum = 0;
+// 	for (var i = 1; i <= iterationAmount; i++) {
+// 	  var r = 1 / (i*(i+1)*(i+2)*(i+3));
+// 	  sum += r;
+// 	}
+// 	return sum;
+// }
+// 
+// var i = 1;
+// do {
+// 	result = fack(i);
+// 	i++;
+// } while (result < sample);
+// 
+// console.log(result, i);
