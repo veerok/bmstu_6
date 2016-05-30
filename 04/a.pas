2061d20ec0f639f940c76cd3b83874d995fc20d4
veerok <vselivanova4@gmail.com>
2016-05-30 19:56:40 +0400

    Update a.pas

--- 04/a.pas
+++ 04/a.pas
@@ -7,12 +7,10 @@ uses math;
 var
   i: integer;
   y: double;
-  tmp: double;
 
 begin
   y := 0;
   for i:=21 downto 1 do begin
-    tmp := y;
     y := sqrt(y + i * 3);
     writeLn(i * 3, tmp, y, ' ');
   end;
